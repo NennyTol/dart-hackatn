aff3397a264d350b9bdd98e334b8dd9cb118a2b3
NennyTol <159932482+NennyTol@users.noreply.github.com>
2024-03-19 10:38:58 +0100

    Add files via upload

---
+++ dart hackaton 1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "IZUNOBI JOY CHINENYE";
+  int age = 26;
+  String school = "NNAMDI AZIKIWE UNIVERSITY AWKA ANAMBRA STATE";
+  String hobby = "LEARNING NEW THINGS";
+  
+  print("MY NAME IS $name. I AM $age YEARS OLD, I AM A GRADUATE OF $school, AND MY HOBBY IS $hobby.");
+}
